ee1e80f1a4e0b90975616f2b8d069d0fed1f1a07
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-13 18:56:24 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCCisE-PQ93kSxFj526dlmtBBnk7r4spSU",
+  authDomain: "milk-bottle-tracker.firebaseapp.com",
+  projectId: "milk-bottle-tracker",
+  storageBucket: "milk-bottle-tracker.appspot.com",
+  messagingSenderId: "265806131846",
+  appId: "1:265806131846:web:e8712e547324c04cd09707"
+};
+
+export default firebaseConfig;
